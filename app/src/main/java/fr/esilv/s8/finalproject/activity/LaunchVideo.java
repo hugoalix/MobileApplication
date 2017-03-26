1a39adb4dd25ffcf88f343bf90b7a9e861ca4bfc
hugoalix <hugo_alix@hotmail.com>
2017-03-26 21:06:29 +0200

    Changement design

---
+++ app/src/main/java/fr/esilv/s8/finalproject/activity/LaunchVideo.java
@@ -0,0 +1,16 @@
+package fr.esilv.s8.finalproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.esilv.s8.finalproject.R;
+
+public class LaunchVideo extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_video);
+    }
+}
